61218d55c9d786b6742423d958c4fed2a3729471
Pujan Parekh <pujanparekhgondal@gmail.com>
2026-02-16 23:13:26 +0530

    selection sort

---
+++ Sorting/Algorithms/selectionSort.java
@@ -0,0 +1,16 @@
+class selectionSort {
+    public int[] selectionSort(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            int minIdx = i;
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[minIdx] > nums[j]) {
+                    minIdx = j;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = nums[minIdx];
+            nums[minIdx] = temp;
+        }
+        return nums;
+    }
+}
