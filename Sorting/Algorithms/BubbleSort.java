c0deb09c48bdd8f3b081912bbdf2311344c91398
Pujan Parekh <pujanparekhgondal@gmail.com>
2026-02-16 23:58:27 +0530

    bubble sort

--- Sorting/Algorithms/BubbleSort.java
+++ Sorting/Algorithms/BubbleSort.java
@@ -1,12 +1,17 @@
 class BubbleSort {
     public int[] bubbleSort(int[] nums) {
         for(int i = 0; i < nums.length - 1; i++) {
+            boolean isSwap = false;
             for(int j = 0; j < nums.length - i - 1; j++) {
                 if(nums[j+1] < nums[j]) {
                     int temp = nums[j];
                     nums[j] = nums[j+1];
                     nums[j+1] = temp;
+                    isSwap = true;
                 }
+            }
+            if(isSwap) {
+                break;
             }
         }
         return nums;
