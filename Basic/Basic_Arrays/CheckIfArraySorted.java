e1ad9aeadee5a6ef1d483e3a16ce1e12f82736c2
Pujan Parekh <pujanparekhgondal@gmail.com>
2026-02-19 19:52:30 +0530

    check if array sorted

---
+++ Basic/Basic_Arrays/CheckIfArraySorted.java
@@ -0,0 +1,10 @@
+class Solution {
+    boolean arraySortedOrNot(int[] arr) {
+       for(int i = 0; i < arr.length - 1; i++) {
+        if(arr[i+1] < arr[i]) {
+            return false;
+        }
+       }
+       return true;
+    }
+}
