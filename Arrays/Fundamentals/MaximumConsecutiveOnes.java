c66ecbc5430588b9f48db15247929b13506f06b8
Pujan Parekh <pujanparekhgondal@gmail.com>
2026-02-19 20:44:58 +0530

    max consecutive ones

---
+++ Arrays/Fundamentals/MaximumConsecutiveOnes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxConsecutiveOnes = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 1) {
+                int currConsecutive = 0;
+                while(i < nums.length && nums[i] == 1) {
+                    currConsecutive++;
+                    i++;
+                }
+                maxConsecutiveOnes = Math.max(currConsecutive, maxConsecutiveOnes);
+            }
+        }
+        return maxConsecutiveOnes;
+    }
+}
