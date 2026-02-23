eb043cfb054cea177665b9da1fd99a0cb1657b5f
Pujan Parekh <pujanparekhgondal@gmail.com>
2026-02-23 20:03:18 +0530

    move zeroes to end

---
+++ Arrays/Logic_Building/MoveZeroesToEnd.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        int j = 0;
+
+        while(i < nums.length && j < nums.length) {
+            while(j < nums.length && nums[j] != 0) {
+                j++;
+            }
+            
+            i = j + 1;
+
+            while(i < nums.length && nums[i] == 0) {
+                i++;
+            }
+
+            if(i < nums.length && j < nums.length) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j++;
+            }
+        }
+    }
+}
