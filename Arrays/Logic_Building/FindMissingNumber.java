0084eef74b7377f351fc3c9ec2ec1494a69fd078
Pujan Parekh <pujanparekhgondal@gmail.com>
2026-02-24 11:48:03 +0530

    find missing number

---
+++ Arrays/Logic_Building/FindMissingNumber.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int arraySum = 0;
+        int sumofN = (nums.length) * (nums.length + 1) / 2;
+
+        for(int i = 0; i < nums.length; i++) {
+            arraySum += nums[i];
+        }
+
+        return sumofN - arraySum;
+    }
+}
