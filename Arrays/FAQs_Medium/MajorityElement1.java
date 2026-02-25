b17cb5f1ef40068ce87ad59e9522fc887caf310e
Pujan Parekh <pujanparekhgondal@gmail.com>
2026-02-25 19:17:30 +0530

    majority element 1

---
+++ Arrays/FAQs_Medium/MajorityElement1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 1;
+        int majorityEl = nums[0];
+
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == majorityEl) {
+                count++;
+            }
+            else {
+                count--;
+            }
+
+            if(count == 0) {
+                count = 1;
+                majorityEl = nums[i];
+            }
+        }
+
+        return majorityEl;
+    }
+}
