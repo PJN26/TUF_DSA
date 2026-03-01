1ca7f8ef9f84932af8f97aab284e8601e566ef5a
Pujan Parekh <pujanparekhgondal@gmail.com>
2026-03-01 12:37:26 +0530

    rotate matrix by 90 degree

---
+++ Arrays/FAQs_Medium/RotateMatrixBy90Degree.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/rotate-image/description/
+
+class Solution {
+    public void rotateMatrix(int[][] matrix) {
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - j - 1];
+                matrix[i][matrix.length - j - 1] = temp;
+            }
+        }
+    }
+}
