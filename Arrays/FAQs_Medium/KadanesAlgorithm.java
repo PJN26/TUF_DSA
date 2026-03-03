a4423d820825b32278b968924540c3847e02236d
Pujan Parekh <pujanparekhgondal@gmail.com>
2026-03-03 11:15:45 +0530

    kadanes algorithm

---
+++ Arrays/FAQs_Medium/KadanesAlgorithm.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/maximum-subarray/description/
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int largestSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+            if(currSum > largestSum) {
+                largestSum = currSum;
+            }
+            if(currSum < 0) {
+                currSum = 0;
+            }
+        }
+        return largestSum;
+    }
+}
