a1de2d87116ae8117e50f4fafffc0385f253d304
Pujan Parekh <pujanparekhgondal@gmail.com>
2026-03-06 10:06:31 +0530

    next permutation

---
+++ Arrays/FAQs_Medium/NextPermutation.java
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/next-permutation/description/
+
+class Solution {
+    public void reverse(int[] nums, int si, int ei) {
+        while(si < ei) {
+            int temp = nums[si];
+            nums[si] = nums[ei];
+            nums[ei] = temp;
+            si++;
+            ei--;
+        }
+    }
+    public void nextPermutation(int[] nums) {
+        int idx = -1;
+        for(int i = nums.length-2; i >= 0; i--) {
+            if(nums[i] < nums[i+1]) {
+                idx = i;
+                break;
+            }
+        }
+        if(idx == -1) {
+            reverse(nums, 0, nums.length - 1);
+            return;
+        }
+        for(int i = nums.length - 1; i > idx; i--) {
+            if(nums[i] > nums[idx]) {
+                int temp = nums[i];
+                nums[i] = nums[idx];
+                nums[idx] = temp;
+                break;
+            }
+        }
+
+        reverse(nums, idx + 1, nums.length - 1);
+    }
+}
