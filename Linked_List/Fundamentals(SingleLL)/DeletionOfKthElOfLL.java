964d13d7428f760730fcb7925fc4eefe53198c09
PJN26 <pujanparekhgondal@gmail.com>
2026-06-12 17:39:52 +0530

    check for new deivice 2

--- Linked_List/Fundamentals(SingleLL)/DeletionOfKthElOfLL.java
+++ Linked_List/Fundamentals(SingleLL)/DeletionOfKthElOfLL.java
@@ -59,6 +59,7 @@ class ListNode{
     ListNode() { data = 0; next = null; }
     ListNode(int x) { data = x; next = null; }
     ListNode(int x, ListNode next) { data = x; this.next = next; }
+}
 
 */
 
