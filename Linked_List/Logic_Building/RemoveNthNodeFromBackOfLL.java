e2f7cd95240042cf8e56fcd2b2d02ad86d141741
PJN26 <pujanparekhgondal@gmail.com>
2026-06-18 20:47:56 +0530

    remove nth node from back of ll

---
+++ Linked_List/Logic_Building/RemoveNthNodeFromBackOfLL.java
@@ -0,0 +1,47 @@
+//https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+
+/*Definition for Singly Linked List
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+ */
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head == null) return null;
+        ListNode fast = head;
+        for(int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        if(fast == null) return head.next;
+
+        ListNode slow = head;
+
+        while(fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return head;
+    }
+}
