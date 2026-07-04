c7d1b653b690e976ddd8040bf143a9811594e5b2
PJN26 <pujanparekhgondal@gmail.com>
2026-07-04 07:34:23 +0530

    single number1

---
+++ Bit_Manipulation/Problems/SingleNumber1.java
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/single-number/description/
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int single = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            single ^= nums[i];
+        }
+
+        return single;
+    }
+}
