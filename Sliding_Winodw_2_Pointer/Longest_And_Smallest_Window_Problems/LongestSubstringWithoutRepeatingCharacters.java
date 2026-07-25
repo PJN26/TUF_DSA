b49af52ad7ca87607abf6bcac3567f725c0a0c0e
PJN26 <pujanparekhgondal@gmail.com>
2026-07-25 10:32:19 +0530

    longest substring without repeating

---
+++ Sliding_Winodw_2_Pointer/Longest_And_Smallest_Window_Problems/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int first0 = -1;
+        int maxConsicutive = 0;
+        int l = 0;
+        int r = 0;
+
+        while(r < nums.length) {
+            if(nums[r] == 0) {
+                
+            }
+        }
+    }
+}
